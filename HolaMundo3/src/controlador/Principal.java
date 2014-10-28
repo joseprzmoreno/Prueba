22282757f94cee08d510baa24af0ac03dc9f0cab
joseprzmoreno <alumno.739498@ies-azarquiel.es>
2014-10-28 21:48:50 +0100

    Punto de partida.

---
+++ HolaMundo3/src/controlador/Principal.java
@@ -0,0 +1,19 @@
+package controlador;
+
+import modelo.Saluda;
+
+public class Principal {
+	
+	public Principal() {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Saluda saludo = new Saluda();
+		saludo.diHola("Jose");
+
+	}
+
+}
---
+++ HolaMundo3/src/modelo/Saluda.java
@@ -0,0 +1,16 @@
+package modelo;
+
+public class Saluda {
+	
+	public Saluda() {
+		
+	}
+		
+    public void diHola(String nombre) {
+		System.out.println("Hola que tal estás " + nombre);
+	}
+    
+}
+	
+
+
